1bac05048c1b727086c003be52436fc55f9817ea
Kenneth Corrales <kcorrales33@gmail.com>
2022-02-20 20:33:35 -0600

    ejercicio 2

--- Tarea.rkt
+++ Tarea.rkt
@@ -3,25 +3,42 @@
 (define lista (list "a" "b" "c" "d" ))
 (define lista2 '())
 
-(define (listN n x)
-  "Repeat x n times."
-  (cond [(zero? n) empty]
-        [else (cons x (listN (sub1 n) x))]))
+(define (listN n x);create a list of n elements x 
+    (
+      cond [(zero? n) empty]
+        [else (cons x (listN (sub1 n) x))]
+    )
+)
+
 
 (
-    define (duplicate lst)
+define (duplicate lst)
     (
-        define (tail lst lstAux)
+    define (tail lst lstAux)
             (
                 cond
                 [(empty? lst) lstAux]
                 [else 
-                ;(define listTemp (list(car lst) (car lst)))
                 ( tail (rest lst)(append lstAux (listN 2 (car lst))))]
             )    
     )
     (tail lst '())
 )
 
-
-(define )
+(define (pack lst)
+    (      
+    define (packAux lst count ele listAux)
+        (
+            cond
+            [(empty? lst) (list listAux (listN count ele))]
+            [else 
+                (
+                    if (= ele (car lst)) 
+                        (packAux (rest lst) (+ count 1) ele listAux ) ; si cumple devuelve esto
+                        (packAux  (rest lst) 1 (car lst) (list listAux (listN count ele))); si no devuelve esto
+                )
+            ]
+        )      
+    )
+    (packAux lst 1 (car lst) '())
+)
