7712fc44757cd1892d697105acd422f5a6309ada
Kenneth Corrales <kcorrales33@gmail.com>
2022-02-22 22:57:26 -0600

    codigo en limpio

--- n-ario.rkt
+++ n-ario.rkt
@@ -2,28 +2,18 @@
 (require racket/struct)
 
 (define-struct Node(id name value children)#:transparent #:mutable); transparent hace que se pueda printear el struct
-(define nodito1 (make-Node 1 'nodito1 11 empty ))
-(define nodito2 (make-Node 2 'nodito2 12 empty ))
-(define lista (list nodito1 nodito2))
-(define nodito3 (Node 3 'nodito3 13 lista))
 
 ; para acceder a los campos del struct se llaman usando la siguiente funcion
 ; (nombreStruct-nombreDelAtributo definicion)
 ; ejemplo para obtener el nombre del nodito: (Node-name nodito)
 
+
+;Auxiliares
 (define (append-element lst elem)
   (foldr cons (list elem) lst))
 
-(define (search list value)
- (cond
-  [(empty? list) false]
-  [(= (Node-value(first list)) value) true]
-  [else (search (rest list) value)]
-  )
-)
-
 (define (delete-element lst elem)
-  (foldr remove (list elem) lst))
+  (remove elem lst))
 
 (define (insertNode parent child)
    (set-Node-children! parent (append-element (Node-children parent) child))
@@ -32,10 +22,10 @@
 (define (deleteNode parent child)
    (set-Node-children! parent (delete-element (Node-children parent) child))
 )
-
+;;;
 
   
-
+;Funcion para insertar el nodo
 (define (insert root parent id name value)
     (if (equal? parent (Node-value root))
         (insertNode root (make-Node id name value empty)) ; Si estoy en el nodo correcto
@@ -46,22 +36,24 @@
     )
 )
 
-(define (find root value)
-    (if (equal? value (Node-value root))
+
+;Funcion busqueda de los nodos 
+(define (find-node root value)
+    (if (equal? (Node-value value) (Node-value root))
         (print (list (Node-id root) (Node-name root) (Node-value root) )) ; Si estoy en el nodo correcto
         (
             for( [tmpNode (Node-children root)]); ingrese en los subnodos
-                    (find tmpNode value)
+                    (find-node tmpNode value)
         )
     )
 )
 
 
-
+;Esta la funcion para buscar el ancestro
 (define (ancestor root value) 
     (
     define (ancestorAux root value parent)
-        (if (equal? value (Node-value root))
+        (if (equal? (Node-value value) (Node-value root))
             (print (list (Node-id parent) (Node-name parent) (Node-value parent) )) ; Si estoy en el nodo correcto
             (
                 for( [tmpNode (Node-children root)]); ingrese en los subnodos
@@ -78,18 +70,12 @@
     (
     define (delete-node-aux root node parent)
         (if (equal? (Node-value node) (Node-value root))
-            (deleteNode parent node) ; Si estoy en el nodo correcto
+            (deleteNode parent root) ; Si estoy en el nodo correcto
             (
                 for( [tmpNode (Node-children root)]); ingrese en los subnodos
                         (delete-node-aux tmpNode node root)
             )
         )
     )
-  (ancestorAux root value null)
-  (root)
+  (delete-node-aux root node null)
 )
-
-(insert nodito3 12 77 'Maria 77)
-(insert nodito3 12 88 'Jose 88)
-(insert nodito3 88 99 'Milton 99)
-(insert nodito3 99  22 'Marco 22) 
